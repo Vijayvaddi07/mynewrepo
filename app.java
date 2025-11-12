ad87a5c0d99db9b1e5a8db0685894fb7c0b31667
root <root@ip-172-31-6-39.ec2.internal>
2025-11-12 12:39:18 +0000

    now im adding data

--- app.java
+++ app.java
@@ -9,6 +9,7 @@ class Main {
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
+
 }
 
 //dev2 code begins here
@@ -24,3 +25,16 @@ public class MultiplyTwoNumbers {
         System.out.println("The product is: " + product);
     }
 }
+}
+
+
+#new code by dev1 without pull the code
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello, World!");
+    }
+}  
+
